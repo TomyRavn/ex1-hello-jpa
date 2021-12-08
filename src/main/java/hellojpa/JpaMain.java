70957868b6a6b5c49b09ecd1f44060e866595689
TomyRavn <godqwer123@naver.com>
2021-12-08 12:48:22 +0900

    영속성 컨텍스트 - 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -53,7 +53,7 @@ public class JpaMain {
 
             //삭제
             //em.remove(findMember);
-            //수정
+            //수정(따로 persist 등의 작업이 필요없음, Java Collection처럼 처리)
             //findMember.setName("HelloJPA");
 
             tx.commit();                //커밋 필수
@@ -62,11 +62,35 @@ public class JpaMain {
             //em.persist(member) => 영속, em.detach(member) => 준영속(영속성 컨텍스트에서 회원 엔티티 분리), em.remove(member) => 엔티티 삭제
 
             /**
-             * 영속성 컨텍스트의 이점
+             * < 영속성 컨텍스트의 이점 >
              * 1. 1차 캐시
+             * ==> DB 바로 조회가 아닌 1차 캐시를 먼저 조회하고, 없을 시 DB에서 조회
+             *     DB에서 조회시에는 1차 캐시에 다시 저장하고, 반환해주기 때문에 재 사용 시 DB 조회가 아니라 1차 캐시를 조회하여
+             *     성능 상에서 이점이 있음
+             *     하지만, JPA는 트랜젝션 단위로 수행하며, 트랜젝션이 끝나면 EntityManager를 반환하기 때문에, 1차 캐시도 초기화되므로
+             *     찰나의 순간에 대한 성능 이점만이 존재
+             *     (* 같은 트랜젝션에서 insert 쿼리의 Entity에 대한 조회 시 select 쿼리가 발생하지 않음,
+             *     동일한 Entity 조회 시 select 쿼리는 1회만 수행 )
+             *
              * 2. 동일성(identity) 보장
+             * ===> java Collection의 똑같은 Reference를 가진 객체의 주소가 동일하듯,
+             *      1차 캐시에서 동작하기 때문에 == (동일) 비교가 true로 보장됨
+             *      (* Repeatable read 등급의 트랜젝션 격리 수준을 DB가 아닌 Application 차원에서 제공)
+             *
              * 3. 트랜젝션을 지원하는 쓰기 지연(transactional write-behind)
+             * Member member1 = new Member(150L, "A");
+             * Member member2 = new Member(160L, "B");
+             *
+             * em.persist(member1);
+             * em.persist(member2);
+             * System.out.println("=====================");
+             *
+             * ===> 최적화를 할 수 있는 여지가 주어짐(batch 처리)
+             *      기본 옵션으로 성능에 이점을 얻기 쉬움
+             *
              * 4. 변경 감지(Dirty Checking)
+             * ===> 영속성 컨텍스트에는 Entity와 스냅샷(DB에서 읽어왔던 초기 값)이 존재, 둘 을 비교해서 변경 감지 후 Update
+             *
              * 5. 지연 로딩(Lazy Loading)
              */
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -14,6 +14,19 @@ public class Member {
     // @Column(name = "username")
     private String name;
 
+    /** constructor */
+    //※ 참고 : https://1-7171771.tistory.com/123
+    //하이버네이트는 내부적으로 Class.newInstance()라는 리플렉션을 이용해 해당 Entity의 기본 생성자를 호출해서 객체를 생성하게 되는데,
+    //이 리플렉션은 생성자의 매개변수를 읽을 수가 없어서 반드시 기본 생성자를 정의해 줘야 한다.
+    //인자가 있는 생성자를 정의하지 않았다면 자바에서 자동으로 기본 생성자를 생성하기 때문에 따로 명시적으로 기본 생성자를 정의하지 않아도 된다.
+    public Member() {}
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /** getter, setter */
     public Long getId() {
         return id;
     }
